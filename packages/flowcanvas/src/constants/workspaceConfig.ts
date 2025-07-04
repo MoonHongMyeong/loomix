7dc6145c7eb2a3b824e35402cd6a33c937663b10
MoonHongMyeong <moonhongmyeong@gmail.com>
2025-07-04 15:53:11 +0900

    refactor(types): remove zoom limits from WorkspaceConfig and move to constants

---
+++ packages/flowcanvas/src/constants/workspaceConfig.ts
@@ -0,0 +1,4 @@
+export const ZOOM_LIMITS = {
+    min: 0.25,
+    max: 2
+} as const;
--- packages/flowcanvas/src/types/workspace.ts
+++ packages/flowcanvas/src/types/workspace.ts
@@ -1,9 +1,6 @@
 import type { Point, Size } from "./common";
 
-export type WorkspaceConfig = Size & {
-    minZoom: number;
-    maxZoom: number;
-}
+export type WorkspaceSize = Size
 
 export type WorkspaceViewPort = {
     pan: Point;
